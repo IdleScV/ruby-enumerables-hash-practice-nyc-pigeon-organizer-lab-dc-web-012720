75ad132a35e221e62afdbd9f67e763ba5b88c4d2
Wesley Chen <weschen1996@gmail.com>
2020-01-19 22:50:52 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,6 +1,56 @@
+def livesfind(name, data)
+    arr=[]
+    i = 0
+    while i < data[:lives].keys.length do
+        if data[:lives][(data[:lives].keys[i])].include? name
+            arr.push(data[:lives].keys[i])
+        end
+        i += 1
+    end
+    return arr
+end
+
+def colorfind(name, data)
+    arr=[]
+    i = 0
+    while i < data[:color].keys.length do
+        if data[:color][(data[:color].keys[i])].include? name
+            arr.push(data[:color].keys[i].to_s)
+        end
+        i += 1
+    end
+    return arr
+end
+
 def nyc_pigeon_organizer(data)
-  ans = {}
-  count = 0
-  while count < data.length 
-    data[count]
+    pidgeonNames = data[:gender][:male] + data[:gender][:female]
+    count = 0
+    newdata = {}
+    lcount = 0
+
+
+    while count < pidgeonNames.length
+        colordata =  colorfind(pidgeonNames[count], data)
+        livesdata = livesfind(pidgeonNames[count], data)
+        newdata[pidgeonNames[count]] = {
+                :color => (
+                    colordata
+
+                ),
+                :gender => [
+                    if count < data[:gender][:male].length
+                        "male"
+                    else
+                        "female"
+                    end
+                ],
+                :lives => (
+                    livesdata
+                )
+            }
+        count += 1
+    end
+
+    return newdata
+
 end
